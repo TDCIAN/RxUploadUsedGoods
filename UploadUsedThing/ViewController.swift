ba5f97b574ae49afa7d9b98efe1bc89f839c4b87
TDCIAN <tdcian71@gmail.com>
2022-04-01 09:20:50 +0900

    config base

---
+++ UploadUsedThing/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  UploadUsedThing
+//
+//  Created by JeongminKim on 2022/04/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
